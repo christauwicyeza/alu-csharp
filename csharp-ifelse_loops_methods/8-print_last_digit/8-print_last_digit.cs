824a7c37111c99d25009c09aaf21f171a08857e8
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-16 20:05:10 +0200

    Added last digit printing method in C#

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,16 @@
+﻿public class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        number = Math.Abs(number);
+        int lastDigit = number % 10;
+        Console.WriteLine($"Last digit of {number} is: {lastDigit}");
+        return lastDigit;
+    }
+
+    static void Main()
+    {
+        int testNumber = 4567;
+        int result = PrintLastDigit(testNumber);
+    }
+}
