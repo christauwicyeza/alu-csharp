c6a2280788f374d481aea85c4cea6001fe0b445b
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-16 20:51:03 +0200

    Number Formatting

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -2,11 +2,16 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         for (int i = 0; i < 99; i++)
         {
-            Console.Write($"{i} {(i < 98 ? "," : "\n")}");
+            Console.Write($"{i:D} = 0x{i:X}");
+            if (i < 98)
+            {
+                Console.Write(", ");
+            }
         }
+        Console.WriteLine();
     }
 }
