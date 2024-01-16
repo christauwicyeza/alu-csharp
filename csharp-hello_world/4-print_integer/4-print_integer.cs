46ba63dff73df3e8a52a5d1b5ec93f412b1c4773
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-16 11:14:28 +0200

    print_integer

---
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int number = 972;
+		Console.WriteLine($"{number} Mission Street");
+	}
+}
+
