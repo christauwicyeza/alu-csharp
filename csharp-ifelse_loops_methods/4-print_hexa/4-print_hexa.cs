ad7959a16511fb0b770aacd75348a1a03fe9e94b
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-16 17:30:47 +0200

    added new changes

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 
-class Program{
-    static void Main(String[] args){
-         for(int i = 0; i < 99 ; i++){
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 99; i++)
+        {
             Console.WriteLine($"{i} = 0x{i:X}");
-         }
+        }
     }
 }
