b29eae2e60fc5f7d2e9fcd4158cb6abaf199e636
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-16 15:23:25 +0200

    print_not

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Loop through the alphabet and print each letter (excluding 'q' and 'e') without a newline
+        for (char letter = 'a'; letter <= 'z'; letter++)
+            if (letter == 'q' || letter == 'e')
+            {
+                continue;
+            }
+            Console.Write(letter);
+        }
+    }
