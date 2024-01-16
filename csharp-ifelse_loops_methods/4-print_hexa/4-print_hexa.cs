68244171d1cd58e1e339cc494e643617c51fb055
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-16 23:28:10 +0200

    integer

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -3,9 +3,9 @@ class Program
 {
     static void Main()
     {
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i <= 98; i++)
         {
-            Console.Write($"{i:D2}{(i < 99 ? ", " : "\n")}");
+            Console.WriteLine($"{i} = 0x{i:X}");
         }
     }
 }
