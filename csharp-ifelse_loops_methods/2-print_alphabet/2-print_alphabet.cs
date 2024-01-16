51c9347d7490eb3d9d74adc12ee468af173c2dca
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-16 14:56:13 +0200

    changes

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class Program
+{
+    static void Main(String[] args){
+        for(char letter = 'A'; letter <= 'Z'; letter++){
+           Console.Write(letter);
+        }
+    }
+}
