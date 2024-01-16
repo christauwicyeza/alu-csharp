8cf9aa6936317e4a304a4f9872d870cae12132bd
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-16 16:59:51 +0200

    updates

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program{
+    static void Main(String[] args){
+         for(int i = 0; i < 100 ; i++){
+            
+            if(i == 99){
+                Console.Write($"{i}");
+            }else{
+                Console.Write($"{i:D2}, ");
+            }
+         }
+        }
+    }
