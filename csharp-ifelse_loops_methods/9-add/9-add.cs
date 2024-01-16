b59f7befae1e8ee6195904c0390fbcd183f2e8f1
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-16 20:08:17 +0200

    Integer Addition

---
+++ csharp-ifelse_loops_methods/9-add/9-add.cs
@@ -0,0 +1,16 @@
+﻿public class Number
+{
+    public static int Add(int a, int b)
+    {
+        int result = a + b;
+        return result;
+    }
+
+    static void Main()
+    {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = Add(num1, num2);
+        Console.WriteLine($"Sum of {num1} and {num2} is: {sum}");
+    }
+}
