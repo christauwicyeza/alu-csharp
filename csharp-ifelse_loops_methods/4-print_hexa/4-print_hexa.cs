7a0d7ea070e57fb8de1dc9adceb3340a4aab1be3
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-16 20:54:52 +0200

    Number Formatting

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -7,11 +7,7 @@ class Program
         for (int i = 0; i < 99; i++)
         {
             Console.Write($"{i:D} = 0x{i:X}");
-            if (i < 98)
-            {
-                Console.Write(", ");
-            }
+            Console.Write(i < 98 ? ", " : "\n");
         }
-        Console.WriteLine();
     }
 }
