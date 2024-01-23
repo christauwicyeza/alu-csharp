c27d8f154bed6bf333d4741128bf679fc803e3d7
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-23 21:22:10 +0200

    changes

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -4,7 +4,7 @@ public class Array
 {
     public static void Reverse(int[] array)
     {
-        if (array == null || array.Length == 0)
+        if (array === null || array.Length == 0)
         {
             Console.WriteLine();
             return;
