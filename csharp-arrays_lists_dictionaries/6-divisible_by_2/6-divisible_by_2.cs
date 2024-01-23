8a25970d2be252ad912296e18947b670ff879581
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-24 00:59:25 +0200

    division

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ListMethods
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        if (myList == null)
+        {
+            Console.WriteLine("Input list is null");
+            return null;
+        }
+
+        List<bool> result = new List<bool>();
+
+        foreach (int number in myList)
+        {
+            bool isMultipleOf2 = (number % 2 == 0);
+            result.Add(isMultipleOf2);
+        }
+
+        return result;
+    }
+}
