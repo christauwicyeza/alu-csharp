d796b811dc091763bdcab45940db72711f9f4dbc
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-23 19:15:03 +0200

    updated changes

--- csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -1,35 +1,14 @@
-﻿using System;
+﻿using System.Collections;
 
-class Array
-{
-    public static int[] CreatePrint(int size)
-    {
-        if (size == 0)
-        {
-            Console.WriteLine();
-            return new int[0];
-        }
-        else if (size < 0)
-        {
-            Console.WriteLine("Size cannot be negative");
-            return null;
+class Array{
+    public static int elementAt(int[] array, int index){
+        int size = array.Length;
+        if(index >= 0 && index < size){
+             return array[index];
+        }else{
+            Console.WriteLine("Index out of range");
+            return -1;
         }
 
-        int[] NumberList = new int[size];
-        for (int i = 0; i < size; i++)
-        {
-            NumberList[i] = i;
-            if (i < size - 1)
-            {
-                Console.Write($"{i} ");
-            }
-            else
-            {
-                Console.Write($"{i}");
-            }
-        }
-        Console.WriteLine();
-
-        return NumberList;
     }
 }
