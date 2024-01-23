be26fb2648eafd4ee4b7a4852e4391488931f54a
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-24 00:04:42 +0200

    new changes

--- csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -1,23 +1,29 @@
-﻿﻿using System.Collections;
+﻿using System;
+using System.Collections.Generic;
 
-class List{
-
-    public static List<int> CreatePrint(int size){
-        
-        if(size < 0){
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
             Console.WriteLine("Size cannot be negative");
             return null;
-        }else{
+        }
+        else
+        {
             List<int> CacheList = new List<int>();
-            for(int i = 0; i < size ; i ++){
+            for (int i = 0; i < size; i++)
+            {
                 Console.Write(i);
                 CacheList.Add(i);
+
+                if (i < size - 1)
+                {
+                    Console.Write(" ");
+                }
             }
-            
-            if (i < size - 1)
-            {
-                Console.Write(" ");
-            }
+
             Console.WriteLine();
             return CacheList;
         }
