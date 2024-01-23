682b9aced5894cf9240f2436ca4fb059ec64ee2f
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-24 00:10:50 +0200

    max interger

--- csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class ListMethods
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        if (myList == null)
+        {
+            Console.WriteLine("Input list is null");
+            return null;
+        }
+
+        List<bool> result = new List<bool>();
+
+        foreach (int number in myList)
+        {
+            result.Add(number % 2 == 0);
+        }
+
+        return result;
+    }
+}
