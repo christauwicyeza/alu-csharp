ff64ff4596b3469efebd581e1ae67f0d084f1533
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-23 19:29:18 +0200

    uodated changes

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -1,13 +1,24 @@
-﻿using System;
+﻿using System.Collections;
 
-public class Array
-{
-    public static void Reverse(int[] array)
-    {
-        for (int i = array.Length - 1; i >= 0; i--)
-        {
-            Console.Write(array[i] + " ");
-        }
+class Array{
+
+    public static void Reverse(int[] array){
+     
+       if(array != null){
+            int size = array.Length;
+        if(size > 0 && size != null){
+
+            for(int i = size - 1; i >= 0; i--){
+                Console.Write(array[i] + " ");
+                }
+                    Console.WriteLine();
+             }else{
+                Console.WriteLine();
+             }
+       }else{
         Console.WriteLine();
+       }
+       
+     
     }
 }
