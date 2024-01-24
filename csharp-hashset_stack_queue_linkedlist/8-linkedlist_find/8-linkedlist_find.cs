2470174f2d5a493e3bba499a2f6b691c8372dde8
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-24 17:53:19 +0200

    8

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        if (myLList == null)
+        {
+            return -1;
+        }
+
+        int index = 0;
+        foreach (int item in myLList)
+        {
+            if (item == value)
+            {
+                return index;
+            }
+            index++;
+        }
+
+        
+        return -1;
+    }
+}
