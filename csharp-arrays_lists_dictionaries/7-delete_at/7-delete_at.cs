f2660ade334f88df902f9cfce9699f4a48ee6682
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-24 16:09:52 +0200

    7

--- csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -1,19 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class ListManipulator
+public class List
 {
     public static List<int> DeleteAt(List<int> myList, int index)
     {
         if (index < 0 || index >= myList.Count)
         {
             Console.WriteLine("Index is out of range");
-        }
-        else
-        {
-            myList.RemoveAt(index);
+            return myList; // Return the original list unchanged
         }
 
-        return myList;
+        List<int> updatedList = new List<int>();
+
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (i != index)
+            {
+                updatedList.Add(myList[i]);
+            }
+        }
+
+        myList.Clear(); // Clear the original list
+        foreach (var item in updatedList)
+        {
+            myList.Add(item); // Add back the items from updatedList to the original list
+        }
+
+        return myList; // Return the original list, now modified
     }
 }
