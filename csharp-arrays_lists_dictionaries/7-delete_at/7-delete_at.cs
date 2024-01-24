437b1b3a217c5cac08df07ffc8f8819f4cc4e9e0
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-24 15:10:33 +0200

    number of keys

--- csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -1,32 +1,14 @@
 ﻿using System.Collections;
 
-class List{
+class Dictionary{
 
-    public static List<int> DeleteAt(List<int> myList, int index){
-        int ListSize = myList.Count;
-        List<int> CacheList = new List<int>();
 
-        if(index >= 0 && index < ListSize){
-            for(int i = 0; i < ListSize; i++){
-                if(i == index){
-                   // Console.WriteLine("Value o index is : " + index + " and value of i : " + i) ;
-                   // Console.WriteLine("index found");
-                    continue;
-                }else{
-                       // Console.WriteLine("Adding the number : " + myList[i]);
-                        CacheList.Add(myList[i]);
-                }
-            
-            }
-           // foreach(int temp in CacheList){
-           //         Console.WriteLine("Printing the cache list elements " + temp);
-           // }
-
-            return CacheList;
-        }else{
-            Console.WriteLine("Index is out of range");
-            return myList;
+    public static int NumberOfKeys(Dictionary<string, string> myDict){
+        int count = 0;
+        foreach(var key in myDict){
+            count++;
         }
 
+        return count;
     }
 }
