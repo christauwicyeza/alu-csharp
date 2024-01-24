8181893180de0e08960ec29f0a74927b7e2ea7ed
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-24 02:04:34 +0200

    division

--- csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -1,23 +1,18 @@
 ﻿using System.Collections;
 
-class List
-{
-    public static List<bool> CheckMultiplesOf2(List<int> numbersToCheck)
-    {
-        List<bool> isDivisibleBy2 = new List<bool>();
+class List{
 
-        foreach (int value in numbersToCheck)
-        {
-            if (value % 2 == 0)
-            {
-                isDivisibleBy2.Add(true);
-            }
-            else
-            {
-                isDivisibleBy2.Add(false);
+    public static List<bool> DivisibleBy2(List<int> myList){
+        List<bool> BoolList = new List<bool>();
+
+        foreach(int value in myList){
+            if(value % 2 == 0){
+                BoolList.Add(true);
+            }else{
+                BoolList.Add(false);
             }
         }
 
-        return isDivisibleBy2;
+        return BoolList;
     }
 }
