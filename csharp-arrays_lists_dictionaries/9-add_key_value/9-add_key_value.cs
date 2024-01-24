40569a5dced0f4c292a889fb799b251a14aea066
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-24 16:21:02 +0200

    9

--- csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
+++ csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -5,14 +5,14 @@ public class Dictionary
 {
     public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
     {
-        // Check if the key exists and add or update accordingly
+        
         if (myDict.ContainsKey(key))
         {
-            myDict[key] = value; // Update the existing key's value
+            myDict[key] = value; 
         }
         else
         {
-            myDict.Add(key, value); // Add the new key-value pair
+            myDict.Add(key, value); 
         }
 
         return myDict;
