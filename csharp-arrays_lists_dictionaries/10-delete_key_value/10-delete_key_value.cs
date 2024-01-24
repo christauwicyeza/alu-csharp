a6c5eee5665474c1e03d90860a04fae6d7fbc14a
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-24 16:21:59 +0200

    10

--- csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
+++ csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 
 
-partial class Dictionary{
+class Dictionary{
     public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key){
         Dictionary<string, string> tempDict = myDict;
         if(tempDict.ContainsKey(key)){
