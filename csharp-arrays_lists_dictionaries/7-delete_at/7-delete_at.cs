6c1e68c84c946b3055d442386b5bbc213e9af645
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-01-24 14:32:54 +0200

    new

--- csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -1,32 +1,26 @@
 ﻿using System.Collections;
 
-class List{
-
-    public static List<int> DeleteAt(List<int> myList, int index){
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
         int ListSize = myList.Count;
         List<int> CacheList = new List<int>();
 
-        if(index >= 0 && index < ListSize){
-            for(int i = 0; i < ListSize; i++){
-                if(i == index){
-                    Console.WriteLine("Value o index is : " + index + " and value of i : " + i) ;
-                   Console.WriteLine("index found");
-                    continue;
-                }else{
-                       Console.WriteLine("Adding the number : " + myList[i]);
-                        CacheList.Add(myList[i]);
-                }
-            
-            }
-            foreach(int temp in CacheList){
-                    Console.WriteLine("Printing the cache list elements " + temp);
+        if (index >= 0 && index < ListSize)
+        {
+            for (int i = 0; i < ListSize; i++)
+            {
+                if (i == index) continue;
+                CacheList.Add(myList[i]);
             }
 
             return CacheList;
-        }else{
+        }
+        else
+        {
             Console.WriteLine("Index is out of range");
             return myList;
         }
-
     }
 }
