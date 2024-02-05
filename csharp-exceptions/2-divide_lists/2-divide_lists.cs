6cdce8dbd7c40fd481a26cbf0be2e5a4453c20bc
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-02-05 13:02:22 +0200

    divide list

--- csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -1,36 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections;
+using System;
 
-public class ListUtils
-{
-    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
-    {
-        List<int> divisionResults = new List<int>();
+class List{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength){
+        List<int> result  = new List<int>();
+        int TempHold = 0;
 
-        try
-        {
-            for (int i = 0; i < listLength; i++)
-            {
-                try
-                {
-                    int result = list1[i] / list2[i];
-                    divisionResults.Add(result);
-                }
-                catch (ArgumentOutOfRangeException)
-                {
-                    Console.WriteLine("Out of range");
-                    return new List<int>(); // Return an empty list if either list is too short
-                }
-                catch (DivideByZeroException)
-                {
-                    Console.WriteLine("Cannot divide by zero");
-                    divisionResults.Add(0); // Add 0 to the divisionResults list if division by zero occurs
-                }
+        for(int i = 0; i < listLength ; i++){
+            try{
+                TempHold = list1[i] / list2[i];
+                result.Add(TempHold);
+            }catch(ArgumentOutOfRangeException){
+                Console.WriteLine("Out of range");
+            }catch(DivideByZeroException){
+                Console.WriteLine("Cannot divide by zero");
+                result.Add(0);
             }
         }
-        catch (ArgumentOutOfRangeException)
-        {
-            Console.WriteLine("Out of range");
-        }
 
-        return divisionResults;
+        return result;
+    }
+}
