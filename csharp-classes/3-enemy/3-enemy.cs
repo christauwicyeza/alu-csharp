486596237231bab73b6db400d64daba64ce06814
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-02-13 19:21:10 +0200

    fixed errors

--- csharp-classes/3-enemy/3-enemy.cs
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -1,34 +1,47 @@
 ﻿using System;
 
-namespace Enemies {
+
+namespace Enemies{
+   
     /// <summary>
     /// Zombie is a public class 
     /// </summary>
-    public class Zombie {
+    public class Zombie{
 
-        /// <summary>
-        /// Holds the value of health
-        /// </summary>
-        public int health;
+        // local health variable
+        int health;
+
 
         /// <summary>
         /// Zombie Constructor inits health to zero
         /// </summary>
-        public Zombie() {
+        public Zombie(){
             health = 0;
         }
 
+
         /// <summary>
-        /// Zombie Constructor inits only for int greater than or equal to zero
+        /// Zoombie Constructor inits only for int greater than or equal to zero
         /// </summary>
-        /// <param name="value">The initial health value</param>
-        /// <exception cref="ArgumentException">Thrown when the provided value is less than 0</exception>
-        public Zombie(int value) {
-            if (value < 0) {
+        /// <param name="value"></param>
+        /// <exception cref="ArgumentException"></exception>
+        public Zombie(int value){
+            if( value < 0){
                 throw new ArgumentException("Health must be greater than or equal to 0");
             }
 
             health = value;
         }
+
+        /// <summary>
+        /// Method returns health local variable
+        /// </summary>
+        /// <returns>health</returns>
+        public int GetHealth(){
+            return health;
+        }
+
+
+
     }
 }
