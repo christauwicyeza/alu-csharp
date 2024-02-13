61c061aea6a1a6eda40347e8ca363cc7e57defba
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-02-13 11:50:41 +0200

    csharp-classes

---
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a zombie enemy.
+    /// </summary>
+    public class Zombie
+    {
+        // This class is currently empty.
+    }
+}
