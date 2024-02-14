fa91935580358278c371c81aa9dcac88370b1879
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-02-14 21:45:56 +0200

    first test

---
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,54 @@
+using NUnit.Framework;
+using MyMath;
+
+
+
+namespace Tests{
+
+
+    [TestFixture]
+    public class Test_NumberAddition
+    {
+        private Operations _myMath;
+
+        [SetUp]
+        public void Setup()
+        {
+            _myMath = new Operations();
+        }
+
+        /// <summary>
+        /// Test if addition of two numbers is 4
+        /// </summary>
+        [Test]
+        public void AdditionOperation()
+        {
+            bool state = false;
+          if((Operations.Add(2, 2)) == 4){
+            state = true;
+          }
+            Assert.IsTrue(state, "State is True");
+            
+        }
+
+        /// <summary>
+        /// Test case with argument passsing. 
+        /// </summary>
+        /// <param name="value"></param>
+        //[TestCase(7)]
+        ///[TestCase(7)]
+        //[TestCase(7)]
+        
+        /*
+        public void TestMultiInput(int value){
+            bool state = false;
+            if((Operations.Add(2, 5)) == value){
+                state = true;
+            }
+            Assert.IsTrue(state, "state is True");
+        }
+        */
+
+    }
+}
+
---
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace  MyMath
+{
+    public class Operations
+    {
+
+        /// <summary>
+        /// Returns addition of two integers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int Add(int a , int b){
+            return a + b;
+        }
+    }
+}
+
