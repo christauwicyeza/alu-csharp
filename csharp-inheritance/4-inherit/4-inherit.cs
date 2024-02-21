d673c580175bb90ab1830532792b5bb9914263d6
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-02-21 15:52:52 +0200

    xml documentation

--- csharp-inheritance/4-inherit/4-inherit.cs
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-
-/// <summary>
-/// Represents the most basic form of an animal.
-/// </summary>
-public class Animal
-{
-    // Empty class
+public class Animal{
 }
+public class Dog : Animal{
 
-/// <summary>
-/// Represents a dog, which is a derivative of an animal.
-/// </summary>
-public class Dog : Animal
-{
-    // Empty class
 }
