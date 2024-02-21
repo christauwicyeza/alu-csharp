18733a41b70b5c4053855cb7a21d1a7a641f311a
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-02-21 15:37:39 +0200

    new

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -1,32 +1,72 @@
-﻿using System;
+﻿using System.Reflection;
+using System;
 using System.Collections.Generic;
-using System.Reflection;
 
-public class Obj
+
+/*
+TypeInfo t = typeof(Calendar).GetTypeInfo();
+IEnumerable<PropertyInfo> pList = t.DeclaredProperties;
+IEnumerable<MethodInfo> mList = t.DeclaredMethods;
+
+StringBuilder sb = new StringBuilder();
+
+sb.Append("Properties:");
+foreach (PropertyInfo p in pList)
 {
-    public static void Print(object myObj)
-    {
-        Type type = myObj.GetType();
-        Console.WriteLine($"{type.Name} Properties:");
-        
-        PropertyInfo[] properties = type.GetProperties();
-        foreach (PropertyInfo property in properties)
-        {
-            Console.WriteLine(property.Name);
+
+    sb.Append("\n" + p.DeclaringType.Name + ": " + p.Name);
+}
+sb.Append("\nMethods:");
+foreach (MethodInfo m in mList)
+{
+    sb.Append("\n" + m.DeclaringType.Name + ": " + m.Name);
+}
+
+Console.WriteLine(sb.ToString());
+*/
+
+
+class Obj {
+
+
+    public static void Print(object myObj){
+
+        TypeInfo t =  myObj.GetType().GetTypeInfo();
+        // IEnumerable<PropertyInfo> pList  = t.DeclaredProperties;
+        IEnumerable<PropertyInfo> pList = t.GetProperties();
+        //Console.WriteLine(pList.First().DeclaringType.Name + " Properties:");
+        Console.WriteLine($"{t.Name} Properties:");
+ 
+        //StringBuilder sb = new StringBuilder();
+        //sb.Append(pList.First().DeclaringType.Name + " Properties:");
+
+
+
+        foreach(PropertyInfo p in pList){
+         //if(p.Attributes == None){
+
+          //  }
+            Console.WriteLine(p.Name);
+           // sb.Append("\n" + p.Name);
         }
 
-        Console.WriteLine($"{type.Name} Methods:");
-        
-        MethodInfo[] methods = type.GetMethods();
-        var seenMethods = new HashSet<string>();
-        
-        foreach (MethodInfo method in methods)
-        {
-            // Exclude special names (property getters/setters, etc.) and avoid duplicates
-            if (!method.IsSpecialName && seenMethods.Add(method.Name))
-            {
-                Console.WriteLine(method.Name);
+        IEnumerable<MethodInfo> pMethod = t.GetMethods();
+        // Console.WriteLine(pMethod.First().DeclaringType.Name + " Methods:");
+        // sb.Append(pMethod.First().DeclaringType.Name + " Method:");
+        Console.WriteLine($"{t.Name} Methods:");
+
+        foreach(MethodInfo m in pMethod){
+            if(m.DeclaringType == myObj.GetType()){
+                 Console.WriteLine(m.Name);
             }
+            // Console.WriteLine(m.Name);
+          
+           // sb.Append("\n" + m.Name);
         }
+
+        // Console.WriteLine(sb.ToString());
+
+        
+
     }
 }
