32e470c45fcd4664329767a3edc5197354d0b695
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-02-21 11:56:27 +0200

    fixed errors

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -1,42 +1,61 @@
-﻿using System;
+﻿using System.Reflection;
+using System;
 using System.Collections.Generic;
-using System.Reflection;
-
-class Obj
-{
-    /// <summary>
-    /// Prints the names of the available properties and methods of an object.
-    /// </summary>
-    /// <param name="myObj">The object to inspect.</param>
-    public static void Print(object myObj)
-    {
-        TypeInfo typeInfo = myObj.GetType().GetTypeInfo();
-        IEnumerable<PropertyInfo> properties = typeInfo.DeclaredProperties;
-        IEnumerable<MethodInfo> methods = typeInfo.DeclaredMethods;
-
-        Console.WriteLine($"{typeInfo.Name} Properties:");
-        foreach (PropertyInfo property in properties)
-        {
-            Console.WriteLine(property.Name);
-        }
-
-        Console.WriteLine($"{typeInfo.Name} Methods:");
-        foreach (MethodInfo method in methods)
-        {
-            Console.WriteLine(method.Name);
-        }
-    }
-}
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var num = 10;
-        var myList = new List<int>();
+        // Example object
+        var exampleObject = new ExampleClass();
+        Obj.Print(exampleObject);
 
-        Obj.Print(num);
         Console.WriteLine("-----------------");
+
+        // Example with a built-in type
+        var myList = new List<int>();
         Obj.Print(myList);
+
+        Console.WriteLine("-----------------");
+
+        // Primitive type example
+        int number = 42;
+        Obj.Print(number);
+    }
+}
+
+class ExampleClass
+{
+    public int ExampleProperty { get; set; }
+    public void ExampleMethod()
+    {
+        Console.WriteLine("Method invoked");
+    }
+}
+
+class Obj
+{
+    public static void Print(object myObj)
+    {
+        TypeInfo t = myObj.GetType().GetTypeInfo();
+        IEnumerable<PropertyInfo> pList = t.GetProperties();
+        Console.WriteLine($"{t.Name} Properties:");
+
+        foreach (PropertyInfo p in pList)
+        {
+            Console.WriteLine(p.Name);
+        }
+
+        IEnumerable<MethodInfo> pMethod = t.GetMethods();
+        Console.WriteLine($"{t.Name} Methods:");
+
+        foreach (MethodInfo m in pMethod)
+        {
+            if (m.DeclaringType == myObj.GetType())
+            {
+                Console.WriteLine(m.Name);
+            }
+        }
     }
 }
