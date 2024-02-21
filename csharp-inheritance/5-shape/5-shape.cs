4f88fd5173f4a2b0aabaffe9809339cab73306d2
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-02-21 16:03:30 +0200

    updated 5-shape

--- csharp-inheritance/5-shape/5-shape.cs
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 
-
-public class Shape{
-
 /// <summary>
-/// Emtpy area calculation
+/// Represents a basic shape.
 /// </summary>
-/// <returns></returns>
-/// <exception cref="NotImplementedException"></exception>
-    public virtual int Area(){
+public class Shape
+{
+    /// <summary>
+    /// Calculates the area of the shape.
+    /// </summary>
+    /// <returns>The area of the shape.</returns>
+    /// <exception cref="NotImplementedException">Thrown when the method is not implemented in a derived class.</exception>
+    public virtual int Area()
+    {
         throw new NotImplementedException("Area() is not implemented");
     }
 }
