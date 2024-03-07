96d2b0498bc659dcfe9f7f7cefe3ebae08d4b13f
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-03-07 16:04:44 +0200

    new changes

--- csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 class VectorMath
 {
@@ -9,11 +10,33 @@ class VectorMath
             return new double[] { -1 };
         }
 
-        return new double[]
+        double[] result = new double[]
         {
             vector1[1] * vector2[2] - vector1[2] * vector2[1],
             vector1[2] * vector2[0] - vector1[0] * vector2[2],
             vector1[0] * vector2[1] - vector1[1] * vector2[0]
         };
+
+        result = result.Select(x => x == -0.0 ? 0.0 : x).ToArray();
+        return result;
+    }
+    
+    public static string FormatVector(double[] vector)
+    {
+        return $"({string.Join(", ", vector.Select(x => x.ToString()))})";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] vector1 = { /* your vector1 elements */ };
+        double[] vector2 = { /* your vector2 elements */ };
+
+        double[] crossProductResult = VectorMath.CrossProduct(vector1, vector2);
+        string formattedResult = VectorMath.FormatVector(crossProductResult);
+
+        Console.WriteLine(formattedResult);
     }
 }
