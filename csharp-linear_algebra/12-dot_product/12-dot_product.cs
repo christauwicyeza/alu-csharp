f622bd23710613d7fe303fa74724101cef75a1ac
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-03-07 12:57:40 +0200

    dot product

--- csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -1,24 +1,16 @@
 ﻿using System;
 
-class VectorMath
-{
-    public static double DotProduct(double[] vector1, double[] vector2)
-    {
-        // Check if both vectors are of the same dimension (2D or 3D) and valid
-        if ((vector1.Length == 2 || vector1.Length == 3) && vector1.Length == vector2.Length)
-        {
-            double dotProduct = 0;
-            // Calculate the dot product
-            for (int i = 0; i < vector1.Length; i++)
-            {
-                dotProduct += vector1[i] * vector2[i];
-            }
-            return dotProduct;
+class VectorMath{
+    public static double DotProduct(double[] vector1, double[] vector2){
+
+        if(vector1.Length == 2 && vector2.Length == 2){
+            return (vector1[0]  * vector2[0]) + (vector1[1] * vector2[1]);
         }
-        else
-        {
-            // Return -1 if the vectors are not both 2D or both 3D or not of the same dimension
-            return -1;
+
+        if(vector1.Length == 3 && vector2.Length == 3){
+            return (vector1[0] * vector2[0]) + (vector1[1] * vector2[1]) + (vector1[2] * vector2[2]);
         }
+
+        return -1;
     }
 }
