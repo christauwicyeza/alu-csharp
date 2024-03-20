2c736a77ee909ddccee1b8f3b909166efed3b2e1
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2024-03-20 19:48:14 +0300

    Updated 3-type_get.cs

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -1,72 +1,29 @@
-﻿using System.Reflection;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 
+    /// <summary>Represents the class Obj.</summary>
 
-/*
-TypeInfo t = typeof(Calendar).GetTypeInfo();
-IEnumerable<PropertyInfo> pList = t.DeclaredProperties;
-IEnumerable<MethodInfo> mList = t.DeclaredMethods;
-
-StringBuilder sb = new StringBuilder();
-
-sb.Append("Properties:");
-foreach (PropertyInfo p in pList)
+public class Obj
 {
-
-    sb.Append("\n" + p.DeclaringType.Name + ": " + p.Name);
-}
-sb.Append("\nMethods:");
-foreach (MethodInfo m in mList)
-{
-    sb.Append("\n" + m.DeclaringType.Name + ": " + m.Name);
-}
-
-Console.WriteLine(sb.ToString());
-*/
-
-
-class Obj {
-
-
-    public static void Print(object myObj){
-
-        TypeInfo t =  myObj.GetType().GetTypeInfo();
-        // IEnumerable<PropertyInfo> pList  = t.DeclaredProperties;
-        IEnumerable<PropertyInfo> pList = t.GetProperties();
-        //Console.WriteLine(pList.First().DeclaringType.Name + " Properties:");
-        Console.WriteLine($"{t.Name} Properties:");
- 
-        //StringBuilder sb = new StringBuilder();
-        //sb.Append(pList.First().DeclaringType.Name + " Properties:");
-
-
-
-        foreach(PropertyInfo p in pList){
-         //if(p.Attributes == None){
-
-          //  }
-            Console.WriteLine(p.Name);
-           // sb.Append("\n" + p.Name);
-        }
-
-        IEnumerable<MethodInfo> pMethod = t.GetMethods();
-        // Console.WriteLine(pMethod.First().DeclaringType.Name + " Methods:");
-        // sb.Append(pMethod.First().DeclaringType.Name + " Method:");
-        Console.WriteLine($"{t.Name} Methods:");
-
-        foreach(MethodInfo m in pMethod){
-            if(m.DeclaringType == myObj.GetType()){
-                 Console.WriteLine(m.Name);
-            }
-            // Console.WriteLine(m.Name);
-          
-           // sb.Append("\n" + m.Name);
-        }
-
-        // Console.WriteLine(sb.ToString());
-
+    /// <summary>
+    /// prints the names of the available properties and methods of an object.
+    /// </summary>
+    /// <param name="myObj">Object from where to get informations.</param>
+    public static void Print(object myObj)
+    {
+        Type type = myObj.GetType();
         
-
+        Console.WriteLine($"{type.Name} Properties:");
+        foreach (PropertyInfo prop in type.GetProperties())
+        {
+            Console.WriteLine(prop.Name);
+        }
+        
+        Console.WriteLine($"{type.Name} Methods:");
+        foreach (MethodInfo method in type.GetMethods())
+        {
+            Console.WriteLine(method.Name);
+        }
     }
 }
