39c32babaecae904a8245b5a8b772bd0acc83336
My Name <c.uwicyeza@alustudent.com>
2024-06-28 00:35:10 +0300

    universal_health

--- csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -1,37 +1,42 @@
 ﻿using System;
 
 /// <summary>
-/// class player
+/// Represents a player with health properties and methods.
 /// </summary>
-public class Player {
-
+public class Player
+{
     private string name;
     private float maxHp;
     private float hp;
 
-/// <summary>
-/// player method
-/// </summary>
-/// <param name="name"></param>
-/// <param name="maxHp"></param>
-    public Player(string name = "Player", float maxHp = 100f) {
+    /// <summary>
+    /// Initializes a new instance of the Player class.
+    /// </summary>
+    /// <param name="name">The name of the player.</param>
+    /// <param name="maxHp">The maximum health points of the player.</param>
+    public Player(string name, float maxHp)
+    {
+        this.name = "Player";
+        this.maxHp = 100f;
+        this.hp = this.maxHp;
 
-        if (maxHp <= 0) {
-            this.maxHp = 100f;
-            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
-        }else {
+        if (maxHp > 0)
+        {
+            this.name = name;
             this.maxHp = maxHp;
+            this.hp = maxHp;
         }
-
-        this.name = name;
-        hp = this.maxHp;
-    }
-
-/// <summary>
-/// printhealth method
-/// </summary>
-        public void PrintHealth() {
-            Console.WriteLine($"{name} has {hp} / {maxHp} health");
-
+        else
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
         }
     }
+
+    /// <summary>
+    /// Prints the current health status of the player.
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
