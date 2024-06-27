347c779e98ccc3b92b0a38c5a1132f5f59bf9c3b
My Name <c.uwicyeza@alustudent.com>
2024-06-27 04:21:15 +0300

    abstract_thinking

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public abstract class Base
+{
+    // Property to store the name
+    public string Name { get; set; }
+
+    // Constructor
+    public Base(string name)
+    {
+        Name = name;
+    }
+
+    // Override ToString() method to return "<name> is a <type>"
+    public override string ToString()
+    {
+        return $"{Name} is a {GetType().Name}";
+    }
+}
