76f01f5dc746e020be719afd611f4157171c5312
My Name <c.uwicyeza@alustudent.com>
2024-06-27 04:29:04 +0300

    xml

--- csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -1,19 +1,45 @@
 ﻿using System;
 
+/// <summary>
+/// Abstract Base class representing an entity with a name.
+/// </summary>
 public abstract class Base
 {
-    // Property to store the name
+    /// <summary>
+    /// Gets or sets the name of the entity.
+    /// </summary>
     public string Name { get; set; }
 
-    // Constructor
+    /// <summary>
+    /// Constructor for initializing the Base class with a name.
+    /// </summary>
+    /// <param name="name">The name of the entity.</param>
     public Base(string name)
     {
         Name = name;
     }
 
-    // Override ToString() method to return "<name> is a <type>"
+    /// <summary>
+    /// Overrides the default ToString method to return a formatted string indicating the type and name.
+    /// </summary>
+    /// <returns>A string representation of the object in the format: "{Name} is a {Type}".</returns>
     public override string ToString()
     {
         return $"{Name} is a {GetType().Name}";
+    }
+}
+
+/// <summary>
+/// Example derived class representing a specific type of Base entity.
+/// </summary>
+public class GardenGate : Base
+{
+    /// <summary>
+    /// Constructor for initializing a GardenGate with a name.
+    /// </summary>
+    /// <param name="name">The name of the garden gate.</param>
+    public GardenGate(string name) : base(name)
+    {
+        // Additional initialization specific to GardenGate if needed
     }
 }
