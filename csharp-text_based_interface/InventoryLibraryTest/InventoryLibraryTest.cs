2506aea220f9fe4101fdb2e66978c1e2cd904764
My Name <c.uwicyeza@alustudent.com>
2024-07-05 10:56:08 +0300

    invetory test

--- csharp-text_based_interface/InventoryLibraryTest/InventoryLibraryTest.cs
+++ csharp-text_based_interface/InventoryLibraryTest/InventoryLibraryTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using InventoryLibrary;
+using System.Collections.Generic;
 
 namespace InventoryLibraryTest
 {
@@ -16,6 +17,26 @@ namespace InventoryLibraryTest
             // Update the timestamp
             baseClass.UpdateTimestamp();
             Console.WriteLine($"Date Updated after update: {baseClass.DateUpdated}");
+            Console.WriteLine();
+
+            // Create an instance of User
+            User user = new User("John Doe");
+            Console.WriteLine($"User ID: {user.Id}, Name: {user.Name}, Date Created: {user.DateCreated}, Date Updated: {user.DateUpdated}");
+            Console.WriteLine();
+
+            // Create an instance of Item
+            Item item = new Item("Laptop", 999.99f)
+            {
+                Description = "A high-end gaming laptop.",
+                Tags = new List<string> { "Electronics", "Computers" }
+            };
+            Console.WriteLine($"Item ID: {item.Id}, Name: {item.Name}, Description: {item.Description}, Price: {item.Price}, Date Created: {item.DateCreated}, Date Updated: {item.DateUpdated}");
+            Console.WriteLine("Tags: " + string.Join(", ", item.Tags));
+            Console.WriteLine();
+
+            // Create an instance of Inventory
+            Inventory inventory = new Inventory(user.Id, item.Id, 5);
+            Console.WriteLine($"Inventory ID: {inventory.Id}, User ID: {inventory.UserId}, Item ID: {inventory.ItemId}, Quantity: {inventory.Quantity}, Date Created: {inventory.DateCreated}, Date Updated: {inventory.DateUpdated}");
         }
     }
 }
