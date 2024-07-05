ddc078e7db686c3e2ccbb45e948f74fa316aff2c
My Name <c.uwicyeza@alustudent.com>
2024-07-05 10:26:29 +0300

    invetory test

---
+++ csharp-text_based_interface/InventoryLibraryTest/InventoryLibraryTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using InventoryLibrary;
+
+namespace InventoryLibraryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instance of BaseClass
+            BaseClass baseClass = new BaseClass();
+            Console.WriteLine($"ID: {baseClass.Id}");
+            Console.WriteLine($"Date Created: {baseClass.DateCreated}");
+            Console.WriteLine($"Date Updated: {baseClass.DateUpdated}");
+
+            // Update the timestamp
+            baseClass.UpdateTimestamp();
+            Console.WriteLine($"Date Updated after update: {baseClass.DateUpdated}");
+        }
+    }
+}
